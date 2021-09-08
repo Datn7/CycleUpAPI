ce1c5fbca7504061fe47b4df54d66460d8e287ce
Datn7 <datunashvili72@gmail.com>
2021-09-08 08:49:04 +0400

    inits

--- Startup.cs
+++ Startup.cs
@@ -20,7 +20,7 @@ namespace CycleUpAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }
+        } 
 
         public IConfiguration Configuration { get; }
 
